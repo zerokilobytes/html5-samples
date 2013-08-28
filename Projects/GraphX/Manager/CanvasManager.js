222e3016cdd1ea8a037e66f805ac28b67fdfdaf9
zerobytes <markel.mairs@gmail.com>
2013-08-27 20:11:47 -0400

    Added Kinetic

--- Projects/GraphX/Manager/CanvasManager.js
+++ Projects/GraphX/Manager/CanvasManager.js
@@ -1,13 +1,32 @@
 var CanvasManager = function(){
     this.canvas = null;
+    this.stage = null;
+    this.graphLayer = null;
+    
+    this.init();
 };
 
 CanvasManager.prototype = {
-    getCanvas : function(){
-        var canvas = document.getElementById('graph-canvas');
+	init : function(){
+		this.stage = new Kinetic.Stage({
+		    container: 'graphx_canvas',
+		    width: 600,
+		    height: 400
+		  });
+		
+		this.graphLayer = new Kinetic.Layer();
+		
+		this.stage.add(this.graphLayer);
+	},
+    getCanvas2D : function(){
+        var canvas = document.getElementById('graphx_canvas');
         var context = canvas.getContext('2d');
         return context;
-    }
+    },
+    getStage: function(){
+		return this.stage;
+	},
+	getGraphLayer: function(){
+		return this.graphLayer;
+	}
 };
-
-
